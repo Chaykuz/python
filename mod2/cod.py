7bd3ecab29aab1cf0a3f85bef8c39d46d65f3fc7
Nick <chaykuzz@gmail.com>
2022-09-21 00:29:07 +0300

    md2

---
+++ mod2/cod.py
@@ -0,0 +1,35 @@
+# client = 'Петя'
+# pet = 'Кот'
+# print(client)
+# print(' и ')
+# print(pet)
+
+# r, g, b = 'Red', 'Green', 'Blue'
+
+# print(r, b, g, r + g + b, b, g + b)
+
+# first_animal = 'Заяц'
+# second_animal = 'Черепаха'
+
+# print(first_animal, 'спит,', second_animal, 'идёт')
+
+# first_name = input('Введите имя пользователя: ')
+# greeting = 'Утро доброе'
+# print(greeting, first_name)
+# intro = "К сожалению, у Вас нет доступа к системе."
+# info = "Пожалуйста, обратитесь к системному администратору."
+# print(intro)
+# print(info)
+
+# dep_city = input('Город вылета: ')
+# arive_city = input('Город прилёта: ')
+
+# print(dep_city, '—', arive_city)
+
+a = input('Введите первое слово: ')
+b = input('Введите второе слово: ')
+print(a, b)
+c = a 
+a = b
+b = c
+print(a, b)
---
+++ mod2/kon.py
@@ -0,0 +1,9 @@
+# name = input('Введите имя: ')
+# print ('Привет, ' + name + '!')
+
+a = input('Часть 1: ')
+b = input('Часть 2: ')
+
+c = a + ' ' + b
+
+print(c)
