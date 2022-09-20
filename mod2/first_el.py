e82691b90c61900714d9e96827f09f6dc85a3a86
Nick <chaykuzz@gmail.com>
2022-09-20 23:37:19 +0300

    first commit

---
+++ mod2/first_el.py
@@ -0,0 +1,29 @@
+# z = 'Моя первая переменная!'
+# y = 'И не последняя!'
+
+# print (z)
+# print (y)
+
+
+# my_color = 'Зелёный'
+# sport= 'Бег'
+# name = 'Иван'
+# print('Меня зовут')
+# print(name)
+# print('Мой любимый цвет:')
+# print(my_color)
+# print('Мой любимый вид спорта:')
+# print(sport)
+
+
+# name = input('Введите имя ')
+
+# print ('Привет', name)
+
+# name2 = input('Введите второе имя ')
+
+# print ('Привет', name2)
+
+# surname = input('Введите фамилию: ')
+# print('Ваша фамилия - ')
+# print(surname)
